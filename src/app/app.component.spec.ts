f7dfc451a3c6cfdb27384c74cbe0c8416d2fc4bb
Ronali Senapati <ronalisenapati70@gmail.com>
2025-11-14 07:49:48 +0100

    Added basic structure of unit test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, BrowserAnimationsModule],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
